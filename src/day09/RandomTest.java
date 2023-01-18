a97b219d1655d540ad7859d95a7ffd7677f1b476
Jonghak Kim <whdgkr9070@naver.com>
2023-01-18 20:57:27 +0900

    2023.01.18 7~9강 학습

---
+++ src/day09/RandomTest.java
@@ -0,0 +1,17 @@
+package day09;
+
+import java.util.Random;
+
+public class RandomTest {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println((int) (Math.random() * 6) + 1);
+		}
+		System.out.println("================");
+		Random random = new Random();
+		for (int i = 1; i <= 6; i++) {
+			System.out.println(random.nextInt(45) + 1);
+		}
+	}
+}
